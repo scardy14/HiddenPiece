2f9887947007b23d1841b5f3adde43f865d0526b
HunjinKim8830 <issachunjin@gmail.com>
2022-12-15 11:22:13 +0900

    change2

--- HiddenPiece/src/test/java/org/goodomen/hiddenpiece/HiddenPieceTest.java
+++ HiddenPiece/src/test/java/org/goodomen/hiddenpiece/HiddenPieceTest.java
@@ -1,6 +1,5 @@
 package org.goodomen.hiddenpiece;
 
-
 import org.goodomen.hiddenpiece.model.mapper.MemberMapper;
 import org.goodomen.hiddenpiece.model.vo.MemberVO;
 import org.junit.jupiter.api.Test;
@@ -11,20 +10,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class HiddenPieceTest {
-	private Logger logger=LoggerFactory.getLogger(getClass());
+	private Logger logger = LoggerFactory.getLogger(getClass());
 	private MemberMapper memberMapper;
-	
+
 	@Autowired
 	public HiddenPieceTest(MemberMapper memberMapper) {
 		super();
 		this.memberMapper = memberMapper;
 	}
+
 	@Test
 	public void login() {
-		MemberVO memberVO=new MemberVO("yerin0110", "a", null, null, null, null, 0, null, null, 0, 0);
-		MemberVO resultVO=memberMapper.login(memberVO);
-		logger.debug("login vo: {}",resultVO);
+		MemberVO memberVO = new MemberVO("yerin0110", "a", null, null, null, null, 0, null, null, 0, 0);
+		MemberVO resultVO = memberMapper.login(memberVO);
+		logger.debug("login vo: {}", resultVO);
 	}
-	
-	
+
 }
