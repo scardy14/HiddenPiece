d559490105021894dc45c2a30673de2444855386
zxcaxaz <gam98846@gmail.com>
2022-12-26 17:13:17 +0900

    221226 kkb 인터셉트 보안

--- HiddenPiece/src/main/java/org/goodomen/hiddenpiece/config/intercepter/HandlerIntercepterConfigure.java
+++ HiddenPiece/src/main/java/org/goodomen/hiddenpiece/config/intercepter/HandlerIntercepterConfigure.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import lombok.RequiredArgsConstructor;
-
-@Configuration	//핸들러 인터셉터 설정 클래스임을 스프링 컨테이너에 알린다
+@Configuration	
 public class HandlerIntercepterConfigure implements WebMvcConfigurer{
 	private final LoginCheckInterceptor loginCheckInterceptor;
 	@Autowired
@@ -17,11 +15,7 @@ public class HandlerIntercepterConfigure implements WebMvcConfigurer{
 	}
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(loginCheckInterceptor).
-		addPathPatterns("/**"). //: /** => 현재 경로(context path)와 하위의 모든 경로에 인터셉터를 적용, 모든 요청에 인터셉터 적용하겠다는 의미 
-		excludePathPatterns("path");//: 인터셉터 적용을 제외할 경로를 명시 ( home , 로그인폼이 있는 경로, 정적 자원정보, 비인증상태에서 서비스 할수 있는 경로 )
-		// /registerMember* : registerMember 문자열로 시작되는 모든 경로 
-		//registry.addInterceptor(loginCheckInterceptor).addPathPatterns().excludePathPatterns();
+
 	registry.addInterceptor(loginCheckInterceptor)
 	.addPathPatterns("/**")
 	.excludePathPatterns("/","/home","/loginForm","/registerForm","/needLogin","/login"
